7570c065c89703f82a7deb9eac4f8d55ebb7f737
Felipe <felipemenezes937@gmail.com>
2026-03-09 11:41:59 -0300

    feat: chamando os componentes do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import Header from "./assets/components/Header"
+import Resultado from "./assets/components/Resultado"
 function App() {
 
 
   return (
     <>
-      
-    </>
+      <Header/>
+      <Resultado/>
+      </>
   )
 }
 
